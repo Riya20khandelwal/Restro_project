3fa2b06f3d2cd1ed7382b7049f4b77d2f7a3f3b9
Riya20khandelwal <riyakhandelwal204@gmail.com>
2024-09-12 17:33:13 +0530

    initail commit

---
+++ core/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class ItemList(models.Model):
+    Category_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.Category_name
+
+class Items(models.Model):
+    Item_name= models.CharField(max_length=20)
+    description = models.TextField(blank=False)
+    Price= models.IntegerField()
+    Category = models.ForeignKey(ItemList, related_name="Name", on_delete=models.CASCADE)
+    Image = models.ImageField(upload_to='items/')
+
+    def __str__(self):
+        return self.Item_name
+
+class AboutUs(models.Model):
+    Description = models.TextField(blank=False)
+
+class Feedback(models.Model):
+    User_name = models.CharField(max_length=25)
+    Description = models.TextField(blank=False)
+    Ratting = models.IntegerField()
+
+    def __str__(self):
+        return self.User_name
+
+
+class BookTable(models.Model):
+    Name= models.CharField(max_length=20)
+    phone_number = models.IntegerField()
+    Email = models.EmailField()
+    Total_person = models.IntegerField()
+    Booking_date = models.DateField()
+
+    def __str__(self):
+        return self.Name
---
+++ core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from core.models import *
+
+def HomeView(request):
+    items = Items.objects.all()
+    list = ItemList.objects.all()
+    return render(request, 'home.html', {
+        'items':items,
+        'list': list,
+    })
+
+def AboutView(request):
+    return render(request, 'about.html')
+
+def MenuView(request):
+    items = Items.objects.all()
+    list = ItemList.objects.all()
+    return render(request, 'menu.html', {
+        'items':items,
+        'list': list,
+    })
+
+def BookTableView(request):
+    return render(request, 'book_table.html')
+
+def FeedbackView(request):
+    return render(request, 'book_table.html')
